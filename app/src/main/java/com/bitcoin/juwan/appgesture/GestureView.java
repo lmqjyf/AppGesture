33f667b2dda478c31b890ae0aa11ebeb3487413b
liumengqiang <235434504@qq.com>
2019-10-29 22:26:08 +0800

    1: 方法添加注释

--- app/src/main/java/com/bitcoin/juwan/appgesture/GestureView.java
+++ app/src/main/java/com/bitcoin/juwan/appgesture/GestureView.java
@@ -28,6 +28,8 @@ public class GestureView extends View {
 
     private static final int circleRadius = 50; //圆半径
 
+    private static final int selectCircleRadius = 25; //内圆的半径
+
     //存储九个点
     private static List<PointCoordinate> coordinateList = new ArrayList<>();
     //存储选中的点
@@ -69,6 +71,10 @@ public class GestureView extends View {
         canvas.drawLine(startX, startY, currencyX, currencyY, paint);
     }
 
+    /**
+     * 连接选中圆的线以及内圆
+     * @param canvas
+     */
     private void drawSelectView(Canvas canvas) {
         PointCoordinate pointCoordinate = null;
         for(Map.Entry<Integer, PointCoordinate> entry : selectMap.entrySet()) {
@@ -76,60 +82,65 @@ public class GestureView extends View {
                 pointCoordinate = entry.getValue();
             } else {
                 PointCoordinate currPoint = entry.getValue();
+                //画线
                 canvas.drawLine(pointCoordinate.x, pointCoordinate.y, currPoint.x, currPoint.y, paint);
                 pointCoordinate = currPoint;
             }
-            canvas.drawCircle(pointCoordinate.x, pointCoordinate.y, 25, paint);
+            //画圆
+            canvas.drawCircle(pointCoordinate.x, pointCoordinate.y, selectCircleRadius, paint);
         }
     }
 
-    float startY;
+    float startY; //触摸开始的坐标点
     float startX ;
 
-    float currencyX;
+    float currencyX; //移动过程中的坐标点
     float currencyY;
 
-    boolean isValid = false;
+    boolean isValid = false; //是否是有效触摸
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN : {
-                startX = event.getX();
-                startY = event.getY();
                 currencyX = event.getX();
                 currencyY = event.getY();
                 int index = checkIsAddPoint();
                 isValid = index > -1 ? true : false;
                 if(isValid) {
+                    startX = coordinateList.get(index).x;
+                    startY = coordinateList.get(index).y;
                     selectMap.put(index, coordinateList.get(index));
                     this.postInvalidate();
                 }
                 break;
             }
             case MotionEvent.ACTION_MOVE : {
-                if(!isValid) {
+                if(!isValid) { //判断触摸点是否有效
                     break;
                 }
+//                if(selectMap.size() == 9) {
+//                    break;
+//                }
                 currencyX = event.getX();
                 currencyY = event.getY();
 
                 int index = checkIsAddPoint();
-
+                //移动过程中有选中的点 && 该点还不在选中的集合中
                 if(index > -1 && !selectMap.containsKey(index)) {
-                    startX = coordinateList.get(index).x;
+                    startX = coordinateList.get(index).x;//重置线的起始点
                     startY = coordinateList.get(index).y;
-                    selectMap.put(index, coordinateList.get(index));
+                    selectMap.put(index, coordinateList.get(index)); //将该起始点放入集合中
                 }
 
                 if(selectMap.size() != 0) {
-                    //重新画图
+                    //异步执行onDraw方法
                     this.postInvalidate();
                 }
                 break;
             }
             case MotionEvent.ACTION_UP : {
-                if(!isValid) {
+                if(!isValid) { //判读触摸点是否有效
                     break;
                 }
                 currencyX = 0;
@@ -144,6 +155,10 @@ public class GestureView extends View {
         return true;
     }
 
+    /**
+     * 判断该触摸点是否在九个触摸圆内
+     * @return
+     */
     private int checkIsAddPoint() {
         for(int index = 0; index < coordinateList.size();) {
             PointCoordinate pointCoordinate = coordinateList.get(index);
@@ -158,6 +173,10 @@ public class GestureView extends View {
         return -1;
     }
 
+    /**
+     * 画9个外圆
+     * @param canvas
+     */
     private void drawCircle(Canvas canvas) {
         paint.setStyle(Paint.Style.STROKE);//设置画笔属性是空心圆
         paint.setStrokeWidth(2); //设置划线的宽度
@@ -166,6 +185,9 @@ public class GestureView extends View {
         }
     }
 
+    /**
+     * 生成9个外圆的中心坐标
+     */
     private void checkLinkMap() {
         if(coordinateList.size() != 0) {
             return;

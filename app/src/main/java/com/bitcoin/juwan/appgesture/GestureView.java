f4f536dd4c0cb82cbcb10dd9f5ccbef4fb41948c
liumengqiang <235434504@qq.com>
2019-10-29 22:11:37 +0800

    1: 逻辑添加

--- app/src/main/java/com/bitcoin/juwan/appgesture/GestureView.java
+++ app/src/main/java/com/bitcoin/juwan/appgesture/GestureView.java
@@ -12,6 +12,7 @@ import android.view.View;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * FileName：GestureView
@@ -25,11 +26,7 @@ public class GestureView extends View {
     private int viewWidth; //宽高
     private int viewHeight;
 
-//    private static final int paddingLength = 20; //padding
-
     private static final int circleRadius = 50; //圆半径
-
-    private boolean isDrawCircle = false; //是否已经画过圆
 
     //存储九个点
     private static List<PointCoordinate> coordinateList = new ArrayList<>();
@@ -66,8 +63,24 @@ public class GestureView extends View {
         //画圆
         drawCircle(canvas);
         paint.setStyle(Paint.Style.FILL);
+        //画选中的线
+        drawSelectView(canvas);
         //手势
         canvas.drawLine(startX, startY, currencyX, currencyY, paint);
+    }
+
+    private void drawSelectView(Canvas canvas) {
+        PointCoordinate pointCoordinate = null;
+        for(Map.Entry<Integer, PointCoordinate> entry : selectMap.entrySet()) {
+            if(pointCoordinate == null) {
+                pointCoordinate = entry.getValue();
+            } else {
+                PointCoordinate currPoint = entry.getValue();
+                canvas.drawLine(pointCoordinate.x, pointCoordinate.y, currPoint.x, currPoint.y, paint);
+                pointCoordinate = currPoint;
+            }
+            canvas.drawCircle(pointCoordinate.x, pointCoordinate.y, 25, paint);
+        }
     }
 
     float startY;
@@ -75,6 +88,8 @@ public class GestureView extends View {
 
     float currencyX;
     float currencyY;
+
+    boolean isValid = false;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -84,53 +99,71 @@ public class GestureView extends View {
                 startY = event.getY();
                 currencyX = event.getX();
                 currencyY = event.getY();
-                this.postInvalidate();
+                int index = checkIsAddPoint();
+                isValid = index > -1 ? true : false;
+                if(isValid) {
+                    selectMap.put(index, coordinateList.get(index));
+                    this.postInvalidate();
+                }
                 break;
             }
             case MotionEvent.ACTION_MOVE : {
+                if(!isValid) {
+                    break;
+                }
                 currencyX = event.getX();
                 currencyY = event.getY();
-                checkIsAddPoint();
-                //重新画图
-                this.postInvalidate();
+
+                int index = checkIsAddPoint();
+
+                if(index > -1 && !selectMap.containsKey(index)) {
+                    startX = coordinateList.get(index).x;
+                    startY = coordinateList.get(index).y;
+                    selectMap.put(index, coordinateList.get(index));
+                }
+
+                if(selectMap.size() != 0) {
+                    //重新画图
+                    this.postInvalidate();
+                }
                 break;
             }
             case MotionEvent.ACTION_UP : {
+                if(!isValid) {
+                    break;
+                }
                 currencyX = 0;
                 currencyY = 0;
                 startX = 0;
                 startY = 0;
+                selectMap.clear();
+                this.postInvalidate();
                 break;
             }
         }
         return true;
     }
 
-    private void checkIsAddPoint() {
+    private int checkIsAddPoint() {
         for(int index = 0; index < coordinateList.size();) {
             PointCoordinate pointCoordinate = coordinateList.get(index);
             double distance = Math.sqrt((currencyX - pointCoordinate.x) * (currencyX - pointCoordinate.x) +
-                    (currencyX - pointCoordinate.y) * (currencyX - pointCoordinate.y));
+                    (currencyY - pointCoordinate.y) * (currencyY - pointCoordinate.y));
             if(distance <= circleRadius) {//选中
-                selectMap.put(index, pointCoordinate);
-                break;
+                return index;
             } else {
                 index ++;
             }
         }
+        return -1;
     }
 
     private void drawCircle(Canvas canvas) {
-//        if(this.isDrawCircle) {
-//            return;
-//        }
-
         paint.setStyle(Paint.Style.STROKE);//设置画笔属性是空心圆
         paint.setStrokeWidth(2); //设置划线的宽度
         for(PointCoordinate point : coordinateList) {
             canvas.drawCircle(point.x, point.y, circleRadius, paint);
         }
-//        this.isDrawCircle = true;
     }
 
     private void checkLinkMap() {
